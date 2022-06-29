57ef6efbe7d3a753689c2f5a0644ef17dc7b130b
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2022-06-29 14:06:03 -0400

    se agrego funcionalidad al boton enviar datos y muestra en consola sin validaciones de vacio

---
+++ js/final.js
@@ -0,0 +1,10 @@
+const enviar = () =>{
+    var nombre = document.getElementById("nombre").value;
+    var correo = document.getElementById("correo").value;
+    var asunto = document.getElementById("asunto").value;
+    var mensaje = document.getElementById("mensaje").value;
+    console.log(nombre);
+    console.log(correo);
+    console.log(asunto);
+    console.log(mensaje);
+}
